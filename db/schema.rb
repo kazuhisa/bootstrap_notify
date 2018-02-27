7dd8087bca645eeb80ea5b1e5c6b20c3ba85f425
Kazuhisa Yamamoto <ak.hisashi@gmail.com>
2018-02-27 20:36:59 +0900

    remove columns

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@ ActiveRecord::Schema.define(version: 20180227083430) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "string"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
